e3d9533558dbaab8280675b9ddb6ea00e5a21f65
CriticalPickle <criticalpicklebiz@gmail.com>
2025-12-20 16:27:10 -0800

    Somewhat fixed data gen for blocks but missing model JSON generation and correct crate item generation

--- src/main/java/com/criticalpickle/criticalcrates/datagen/ModModelProvider.java
+++ src/main/java/com/criticalpickle/criticalcrates/datagen/ModModelProvider.java
@@ -1,16 +1,26 @@
 package com.criticalpickle.criticalcrates.datagen;
 
 import com.criticalpickle.criticalcrates.CriticalCrates;
+import com.criticalpickle.criticalcrates.block.CrateBlock;
+import com.criticalpickle.criticalcrates.block.GlassCrateBlock;
 import com.criticalpickle.criticalcrates.registration.ModBlocks;
 import com.criticalpickle.criticalcrates.registration.ModItems;
+import com.criticalpickle.criticalcrates.util.IDUtils;
+import com.mojang.math.Quadrant;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
-import net.minecraft.client.data.models.MultiVariant;
+import net.minecraft.client.data.models.blockstates.MultiVariantGenerator;
+import net.minecraft.client.data.models.blockstates.PropertyDispatch;
 import net.minecraft.client.data.models.model.ModelTemplates;
-import net.minecraft.client.data.models.model.TexturedModel;
+import net.minecraft.client.renderer.block.model.Variant;
+import net.minecraft.client.renderer.block.model.VariantMutator;
+import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.block.Block;
+
+import java.util.stream.Stream;
 
 public class ModModelProvider extends ModelProvider {
     public ModModelProvider(PackOutput output) {
@@ -35,92 +45,79 @@ public class ModModelProvider extends ModelProvider {
     }
 
     private void axisWithOtherPropertiesCrateBlock(Block block, BlockModelGenerators blockModels) {
-        blockModels.createRotatedPillarWithHorizontalVariant(block, TexturedModel.COLUMN_ALT, TexturedModel.COLUMN_HORIZONTAL_ALT);
-//        PropertyDispatch dispatch = PropertyDispatch.modify(CrateBlock.AXIS).select()
-//        VariantBlockStateBuilder builder = getVariantBuilder(block);
-//        String blockName = block.getDescriptionId().substring(block.getDescriptionId().indexOf("s.") + 2);
-//        ConfiguredModel model;
+        printSelects();
+        String blockName = IDUtils.getItemID(block.asItem());
 
-//        for(Direction.Axis axis : Direction.Axis.values()) {
-//            // Loop through extra properties (add more loops below, but make following code nested)
-//            String path = null;
-//            for(boolean resistance : new boolean[]{false, true}) {
-//                for(boolean lamp : new boolean[]{false, true}) {
-//                    for(boolean lit : new boolean[]{false, true}) {
-//                        for(boolean fire : new boolean[]{false, true}) {
-//                            if(resistance) {
-//                                path = blockName + "_resistant";
-//                            }
-//                            else if(lamp) {
-//                                path = lit ? blockName + "_lamp_on" : blockName + "_lamp";
-//                            }
-//                            else if(fire) {
-//                                path = blockName + "_fireproof";
-//                            }
-//                            else {
-//                                path = blockName;
-//                            }
-//
-//                            if (axis == Direction.Axis.X || axis == Direction.Axis.Z) {
-//                                path += "_horizontal";
-//                            }
-//
-//                            int x = axis == Direction.Axis.Y ? 0 : 90;
-//                            int y = axis == Direction.Axis.X ? 90 : 0;
-//
-//                            if (path.contains("horizontal")) {
-//                                String name = path.substring(0, path.indexOf("_horizontal"));
-//                                if(block instanceof GlassCrateBlock) {
-//                                    model = ConfiguredModel.builder()
-//                                            .modelFile(models().cubeColumnHorizontal(path, modLoc("block/glass/" + name), modLoc("block/glass/" + name + "_top")).renderType("translucent"))
-//                                            .rotationX(x)
-//                                            .rotationY(y)
-//                                            .build()[0];
-//                                }
-//                                else {
-//                                    model = ConfiguredModel.builder()
-//                                            .modelFile(models().cubeColumnHorizontal(path, modLoc("block/wood/" + name), modLoc("block/wood/" + name + "_top")))
-//                                            .rotationX(x)
-//                                            .rotationY(y)
-//                                            .build()[0];
-//                                }
-//                            } else {
-//                                if(block instanceof GlassCrateBlock) {
-//                                    model = ConfiguredModel.builder()
-//                                            .modelFile(models().cubeColumn(path, modLoc("block/glass/" + path), modLoc("block/glass/" + path + "_top")).renderType("translucent"))
-//                                            .rotationX(x)
-//                                            .rotationY(y)
-//                                            .build()[0];
-//                                }
-//                                else {
-//                                    model = ConfiguredModel.builder()
-//                                            .modelFile(models().cubeColumn(path, modLoc("block/wood/" + path), modLoc("block/wood/" + path + "_top")))
-//                                            .rotationX(x)
-//                                            .rotationY(y)
-//                                            .build()[0];
-//                                }
-//                            }
-//
-//                            builder.partialState()
-//                                    .with(CrateBlock.AXIS, axis)
-//                                    .with(CrateBlock.EXPLOSION_RESIST, resistance)
-//                                    .with(CrateBlock.LAMP_UPGRADE, lamp)
-//                                    .with(CrateBlock.LIT, lit)
-//                                    .with(CrateBlock.FIREPROOF, fire)
-//                                    .addModels(model);
-//                        }
-//                    }
-//                }
-//            }
-//        }
+        Identifier baseLoc = Identifier.fromNamespaceAndPath(CriticalCrates.MODID, "block/" + blockName),
+                resistantLoc = Identifier.fromNamespaceAndPath(CriticalCrates.MODID, "block/" + blockName + "_resistant"),
+                lampLoc = Identifier.fromNamespaceAndPath(CriticalCrates.MODID, "block/" + blockName + "_lamp"),
+                lampOnLoc = Identifier.fromNamespaceAndPath(CriticalCrates.MODID, "block/" + blockName + "_lamp_on"),
+                fireLoc = Identifier.fromNamespaceAndPath(CriticalCrates.MODID, "block/" + blockName + "_fireproof");
+        Variant base = new Variant(baseLoc);
+        blockModels.blockStateOutput.accept(
+                MultiVariantGenerator.dispatch(block, BlockModelGenerators.variant(base))
+                        .with(PropertyDispatch.modify(CrateBlock.AXIS)
+                                .select(Direction.Axis.Y, BlockModelGenerators.NOP)
+                                .select(Direction.Axis.Z, BlockModelGenerators.X_ROT_90)
+                                .select(Direction.Axis.X, BlockModelGenerators.X_ROT_90.then(BlockModelGenerators.Y_ROT_90))
+                        )
+                        .with(PropertyDispatch.modify(CrateBlock.EXPLOSION_RESIST)
+                                .select(true, VariantMutator.MODEL.withValue(resistantLoc))
+                                .select(false, BlockModelGenerators.NOP)
+                        )
+                        .with(PropertyDispatch.modify(CrateBlock.LAMP_UPGRADE, CrateBlock.LIT)
+                                .select(true, false, VariantMutator.MODEL.withValue(lampLoc))
+                                .select(true, true, VariantMutator.MODEL.withValue(lampOnLoc))
+                                .select(false, false, BlockModelGenerators.NOP)
+                                .select(false, true, BlockModelGenerators.NOP)
+                        )
+                        .with(PropertyDispatch.modify(CrateBlock.FIREPROOF)
+                                .select(true, VariantMutator.MODEL.withValue(fireLoc))
+                                .select(false, BlockModelGenerators.NOP)
+                        )
+
+        );
+    }
+
+    private static String computePathName(Block block, Direction.Axis axis, boolean resistance, boolean lamp, boolean lit, boolean fire) {
+        String path = null, blockName = IDUtils.getItemID(block.asItem());
+
+        if(resistance) {
+            path = blockName + "_resistant";
+        }
+        else if(lamp) {
+            path = lit ? blockName + "_lamp_on" : blockName + "_lamp";
+        }
+        else if(fire) {
+            path = blockName + "_fireproof";
+        }
+        else {
+            path = blockName;
+        }
+
+        if (axis == Direction.Axis.X || axis == Direction.Axis.Z) {
+            path += "_horizontal";
+        }
+
+        return path;
+    }
+
+    private static Quadrant quadrantFromDegrees(int degrees) {
+        return switch (degrees % 360) {
+            case 0 -> Quadrant.R0;
+            case 90 -> Quadrant.R90;
+            case 180 -> Quadrant.R180;
+            case 270 -> Quadrant.R270;
+            default -> throw new IllegalArgumentException("Invalid rotation: " + degrees);
+        };
     }
 
 //    private void blockItemWithOverrides(Item item) {
 //        String itemName = item.getDescriptionId().substring(item.getDescriptionId().indexOf("s.") + 2);
 //        ItemModelBuilder builder = withExistingParent(itemName, modLoc("block/" + itemName));
-//        ResourceLocation resistanceResLoc = ResourceLocation.tryBuild(CriticalCrates.MODID, "resistant");
-//        ResourceLocation lampResLoc = ResourceLocation.tryBuild(CriticalCrates.MODID, "lamp");
-//        ResourceLocation fireResLoc = ResourceLocation.tryBuild(CriticalCrates.MODID, "fire");
+//        Identifier resistanceResLoc = Identifier.tryBuild(CriticalCrates.MODID, "resistant");
+//        Identifier lampResLoc = Identifier.tryBuild(CriticalCrates.MODID, "lamp");
+//        Identifier fireResLoc = Identifier.tryBuild(CriticalCrates.MODID, "fire");
 //
 //        // Generate other variations
 //        withExistingParent(itemName + "_resistant", modLoc("block/" + itemName + "_resistant"));
@@ -141,4 +138,62 @@ public class ModModelProvider extends ModelProvider {
 //                .model(getExistingFile(modLoc("block/" + itemName + "_fireproof")))
 //                .end();
 //    }
+
+    private static void printSelects() {
+        for (Direction.Axis axis : Direction.Axis.values()) {
+            for (boolean res : new boolean[]{false,true}) {
+                for (boolean lamp : new boolean[]{false,true}) {
+                    for (boolean lit : new boolean[]{false,true}) {
+                        for (boolean fire : new boolean[]{false,true}) {
+
+                            boolean rot;
+                            char directionSymbol;
+                            if(axis == Direction.Axis.X) {
+                                rot = true;
+                                directionSymbol = 'X';
+                            }
+                            else if(axis == Direction.Axis.Z) {
+                                rot = true;
+                                directionSymbol = 'Z';
+                            }
+                            else {
+                                rot = false;
+                                directionSymbol = 'Y';
+                            }
+
+                            if(rot && onlyOne(res, lamp, fire)) {
+                                if(res) {
+                                    System.out.printf(".select(Direction.Axis.%c, %b, %b, %b, %b, %s)%n",
+                                            directionSymbol, res, lamp, lit, fire, "VariantMutator.MODEL.withValue(resistantLoc).then(VariantMutator.X_ROT.withValue(Quadrant.R90))");
+                                }
+                                else if(lamp) {
+                                    if(lit) {
+                                        System.out.printf(".select(Direction.Axis.%s, %b, %b, %b, %b, %s)%n",
+                                                directionSymbol, res, lamp, lit, fire, "VariantMutator.MODEL.withValue(lampOnLoc).then(VariantMutator.X_ROT.withValue(Quadrant.R90))");
+                                    }
+                                    else {
+                                        System.out.printf(".select(Direction.Axis.%s, %b, %b, %b, %b, %s)%n",
+                                                directionSymbol, res, lamp, lit, fire, "VariantMutator.MODEL.withValue(lampLoc).then(VariantMutator.X_ROT.withValue(Quadrant.R90))");
+                                    }
+                                }
+                                else {
+                                    System.out.printf(".select(Direction.Axis.%s, %b, %b, %b, %b, %s)%n",
+                                            directionSymbol, res, lamp, lit, fire, "VariantMutator.MODEL.withValue(fireLoc).then(VariantMutator.X_ROT.withValue(Quadrant.R90))");
+                                }
+                            }
+                            else {
+                                System.out.printf(".select(Direction.Axis.%s, %b, %b, %b, %b, %s)%n",
+                                        directionSymbol, res, lamp, lit, fire, "BlockModelGenerators.NOP");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean onlyOne(boolean res, boolean lamp, boolean fire) {
+        long trueCount = Stream.of(res, lamp, fire).filter(Boolean::booleanValue).count();
+        return trueCount == 1;
+    }
 }
